dad87d2222a39aff2de05ae76ca0a43e4d92210d
Aj <67803315+a6k7rii@users.noreply.github.com>
2024-02-04 21:57:35 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
     bucket         = "personalproj"
-    region         = "US East (Ohio) us-east-2"
+    key            = "personalproj/main"
+    region         = "us-east-2"
     dynamodb_table = "personalproj-dynamo-db-table"
   }
 }
