7151f352dced9e0b99383ecf815af3043cb623ec
Aj <67803315+a6k7rii@users.noreply.github.com>
2024-02-04 21:56:44 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~>  5.35.0"
+      version = "~> 5.35.0"
     }
   }
-  required_version = ">= 1.2.0"
+  #required_version = ">= 1.2.0"
 }
 
 provider "aws" {
